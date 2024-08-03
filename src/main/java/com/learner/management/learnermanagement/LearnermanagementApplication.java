61056356a4f36eb77a7255b3bebf8b368ecd9806
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2024-08-03 12:18:02 +0530

    starter project

---
+++ src/main/java/com/learner/management/learnermanagement/LearnermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.learner.management.learnermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnermanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learner/management/learnermanagement/LearnermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learner.management.learnermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
